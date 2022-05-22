f3c61bfead4e7fa3723ea57045f2beab2593b353
Halal37 <wojciechlidwin37@gmail.com>
2022-05-22 23:45:18 +0200

    Added simple script to scrape movie website

---
+++ SimpleWebScrape in C#/SimpleWebScrape/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SimpleWebScrape
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HtmlAgilityPack.HtmlWeb web = new HtmlAgilityPack.HtmlWeb();
+            HtmlAgilityPack.HtmlDocument doc = web.Load("https://yts.mx");
+            foreach(var i in doc.DocumentNode.SelectNodes("//a[@class='browse-movie-title']"))
+            {
+                Console.WriteLine(i.InnerText);
+            }
+        }
+    }
+}

2baf1dfa261e26930bc4d1c517708a7645e70e89
Halal37 <wojciechlidwin37@gmail.com>
2022-06-23 21:24:43 +0200

    Initial commit

---
+++ src/main/java/first/springboot/project/User.java
@@ -0,0 +1,25 @@
+package first.springboot.project;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@RequiredArgsConstructor
+@ToString
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @NonNull
+    private String username;
+    @NonNull
+    private String password;
+
+
+}
